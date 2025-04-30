6849306ecc4ea7df73d00dd8991f52ec8235b428
dev1-inumera <dev1@i-numera.com>
2025-04-30 12:51:35 +0300

    quotId fix

--- src/pages/Quotes/components/QuoteDetailView.tsx
+++ src/pages/Quotes/components/QuoteDetailView.tsx
@@ -113,7 +113,6 @@ const QuoteDetailView: React.FC<QuoteDetailViewProps> = ({ quote, onBack, onUpda
     try {
       setProcessingPayment(true);
       const response = await createPaymentLink(
-        quote.id,
         Number(quote.totalAmount) * 100, // Montant en centimes
         client.email,
         {
@@ -127,7 +126,7 @@ const QuoteDetailView: React.FC<QuoteDetailViewProps> = ({ quote, onBack, onUpda
             "MVOLA",
             "VISA"
           ],
-          message: "Plaquette d'offres"
+          message: "i-numera"
         }
       );
       
